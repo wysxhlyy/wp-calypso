7aa8dd93d948af1dbe24f9e1ceccf9ebe897b4a1
Bernhard Reiter <ockham@raz.or.at>
2016-12-06 22:45:41 +0100

    PoC

--- client/lib/query-manager/theme/index.js
+++ client/lib/query-manager/theme/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { every, some, includes } from 'lodash';
+import { every, get, includes, keyBy, map, omit, some } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,6 +10,7 @@ import PaginatedQueryManager from '../paginated';
 import ThemeQueryKey from './key';
 import { isPremium } from './util';
 import { DEFAULT_THEME_QUERY } from './constants';
+import { PAGINATION_QUERY_KEYS } from 'lib/query-manager/paginated/constants';
 
 const SEARCH_TAXONOMIES = [ 'subject', 'feature', 'color', 'style', 'column', 'layout' ];
 
@@ -68,6 +69,91 @@ export default class ThemeQueryManager extends PaginatedQueryManager {
 
 			return true;
 		} );
+	}
+
+	/**
+	 * Signal that an item(s) has been received for tracking. Optionally
+	 * specify that items received are intended for patch application, or that
+	 * they are associated with a query. This function does not mutate the
+	 * instance state. Instead, it returns a new instance of QueryManager if
+	 * the tracked items have been modified, or the current instance otherwise.
+	 *
+	 * @param  {(Array|Object)} items              Item(s) to be received
+	 * @param  {Object}         options            Options for receive
+	 * @param  {Boolean}        options.patch      Apply changes as partial
+	 * @param  {Object}         options.query      Query set to set or replace
+	 * @param  {Boolean}        options.mergeQuery Add to existing query set
+	 * @param  {Number}         options.found      Total found items for query
+	 * @return {QueryManager}                      New instance if changed, or
+	 *                                             same instance otherwise
+	 */
+	receive( items = [], options = {} ) {
+		// When tracking queries, remove pagination query arguments. These are
+		// simulated in `PaginatedQueryManager.prototype.getItems`.
+		let modifiedOptions = options;
+		if ( options.query ) {
+			modifiedOptions = Object.assign( {
+				mergeQuery: true
+			}, options, {
+				query: omit( options.query, PAGINATION_QUERY_KEYS )
+			} );
+		}
+
+		const keyedItems = keyBy( items, this.options.itemKey );
+		const nextItems = {
+			...( this.data.items ),
+			...keyedItems
+		};
+		let nextQueries = this.data.queries;
+
+		const receivedQueryKey = this.constructor.QueryKey.stringify( modifiedOptions.query );
+
+		let nextQueryFound;
+		if ( options.found >= 0 && options.found !== get( nextQueries, [ receivedQueryKey, 'found' ] ) ) {
+			nextQueryFound = options.found;
+		}
+
+		// Old query items
+		const nextReceivedQuery = Object.assign( {}, nextQueries[ receivedQueryKey ] );
+		const receivedItemKeys = map( keyedItems, this.options.itemKey );
+		// In reality, we need to splice here
+		const nextItemKeys = [
+			...get( nextReceivedQuery, 'itemKeys', [] ),
+			...receivedItemKeys
+		];
+
+		nextQueries = {
+			...nextQueries,
+			[ receivedQueryKey ]: {
+				itemKeys: nextItemKeys,
+				found: nextQueryFound
+			}
+		};
+
+		console.log( nextReceivedQuery, items, this.options.itemKey, receivedItemKeys, nextQueries, new this.constructor(
+			Object.assign( {}, this.data, {
+				items: nextItems,
+				queries: nextQueries
+			} ),
+			modifiedOptions
+		) );
+
+		// Receive the updated manager, passing a modified set of options to
+		// exclude pagination keys, and to indicate appending query.
+		//const nextManager = super.receive( items, options );
+
+		// If manager is the same instance, assume no changes have been made
+		//if ( this === nextManager ) {
+		//	return nextManager;
+		//}
+
+		return new this.constructor(
+			Object.assign( {}, this.data, {
+				items: nextItems,
+				queries: nextQueries
+			} ),
+			this.options
+		);
 	}
 }
 
