2f3a247c6d33b4597a7ca5f74cf0513b4d4ccc6b
Kelly Dwan <kelly.dwan@gmail.com>
2017-01-20 12:29:43 -0500

    Add space between the comments display sections

--- client/my-sites/site-settings/comment-display-settings/index.jsx
+++ client/my-sites/site-settings/comment-display-settings/index.jsx
@@ -16,27 +16,31 @@ import FormTextInput from 'components/forms/form-text-input';
 const CommentDisplaySettings = ( { translate, onChangeField, submittingForm, fields } ) => {
 	return (
 		<FormFieldset className="comment-display-settings">
-			<FormLabel htmlFor="highlander_comment_form_prompt">{ translate( 'Comments Label' ) }</FormLabel>
-			<FormTextInput
-				name="highlander_comment_form_prompt"
-				type="text"
-				id="highlander_comment_form_prompt"
-				value={ fields.highlander_comment_form_prompt || '' }
-				onChange={ onChangeField( 'highlander_comment_form_prompt' ) }
-				disabled={ submittingForm } />
-			<FormSettingExplanation>
-				{ translate( 'A few catchy words to motivate your readers to comment.' ) }
-			</FormSettingExplanation>
-			<FormLabel htmlFor="jetpack_comment_form_color_scheme">{ translate( 'Color Scheme' ) }</FormLabel>
-			<FormSelect
-				name="jetpack_comment_form_color_scheme"
-				value={ fields.jetpack_comment_form_color_scheme || 'light' }
-				onChange={ onChangeField( 'jetpack_comment_form_color_scheme' ) }
-				disabled={ submittingForm }>
-				<option value="light">{ translate( 'Light' ) }</option>
-				<option value="dark">{ translate( 'Dark' ) }</option>
-				<option value="transparent">{ translate( 'Transparent' ) }</option>
-			</FormSelect>
+			<div className="comment-display-settings__setting">
+				<FormLabel htmlFor="highlander_comment_form_prompt">{ translate( 'Comments Label' ) }</FormLabel>
+				<FormTextInput
+					name="highlander_comment_form_prompt"
+					type="text"
+					id="highlander_comment_form_prompt"
+					value={ fields.highlander_comment_form_prompt || '' }
+					onChange={ onChangeField( 'highlander_comment_form_prompt' ) }
+					disabled={ submittingForm } />
+				<FormSettingExplanation>
+					{ translate( 'A few catchy words to motivate your readers to comment.' ) }
+				</FormSettingExplanation>
+			</div>
+			<div className="comment-display-settings__setting">
+				<FormLabel htmlFor="jetpack_comment_form_color_scheme">{ translate( 'Color Scheme' ) }</FormLabel>
+				<FormSelect
+					name="jetpack_comment_form_color_scheme"
+					value={ fields.jetpack_comment_form_color_scheme || 'light' }
+					onChange={ onChangeField( 'jetpack_comment_form_color_scheme' ) }
+					disabled={ submittingForm }>
+					<option value="light">{ translate( 'Light' ) }</option>
+					<option value="dark">{ translate( 'Dark' ) }</option>
+					<option value="transparent">{ translate( 'Transparent' ) }</option>
+				</FormSelect>
+			</div>
 		</FormFieldset>
 	);
 };
